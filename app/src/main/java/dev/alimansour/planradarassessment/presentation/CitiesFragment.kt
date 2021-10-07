c96325e90b133c3ec546eaad1d293ea88ed067c0
Ali Mansour <dev.ali.mansour@gmail.com>
2021-10-07 09:59:58 +0200

    Initial commit

---
+++ app/src/main/java/dev/alimansour/planradarassessment/presentation/CitiesFragment.kt
@@ -0,0 +1,47 @@
+package dev.alimansour.planradarassessment.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dev.alimansour.planradarassessment.R
+import dev.alimansour.planradarassessment.databinding.FragmentCitiesBinding
+
+/**
+ * WeatherApp Android Application developed by: Ali Mansour
+ * ----------------- WeatherApp IS FREE SOFTWARE -------------------
+ * https://www.alimansour.dev   |   mailto:dev.ali.mansour@gmail.com
+ */
+class CitiesFragment : Fragment() {
+
+    private var _binding: FragmentCitiesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentCitiesBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_CitiesFragment_to_HistoricalFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/dev/alimansour/planradarassessment/presentation/HistoricalFragment.kt
@@ -0,0 +1,47 @@
+package dev.alimansour.planradarassessment.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dev.alimansour.planradarassessment.R
+import dev.alimansour.planradarassessment.databinding.FragmentHistoricalBinding
+
+/**
+ * WeatherApp Android Application developed by: Ali Mansour
+ * ----------------- WeatherApp IS FREE SOFTWARE -------------------
+ * https://www.alimansour.dev   |   mailto:dev.ali.mansour@gmail.com
+ */
+class HistoricalFragment : Fragment() {
+
+    private var _binding: FragmentHistoricalBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHistoricalBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /*binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }*/
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
